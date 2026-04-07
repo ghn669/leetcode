eb570e766bbf134da5f2134369ef2da423675cc8
ghn669 <ghn20061009@qq.com>
2026-04-07 20:22:26 +0800

    20判断有效括号

---
+++ 20.cpp
@@ -0,0 +1,29 @@
+//判断有效括号
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        int len = s.size();
+        for(int i=0;i<len;i++)
+        {
+            if(s[i]=='('||s[i]=='{'||s[i]=='[')
+            {
+                st.push(s[i]);
+                continue;
+            }
+            else {
+                if(st.empty()) return false;
+                char top = st.top();
+                if( (s[i] == ')' && top == '(') ||
+                    (s[i] == '}' && top == '{') ||
+                    (s[i] == ']' && top == '[') ) {
+                    st.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
