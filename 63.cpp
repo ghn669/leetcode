b116e5d2fcf024b151321c8e67c5713b01ebb795
ghn669 <ghn20061009@qq.com>
2026-04-08 21:15:29 +0800

    63有障碍物的不同路径

---
+++ 63.cpp
@@ -0,0 +1,32 @@
+//有障碍物的不同路径
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        for(int i=1;i<=n;i++)
+        {
+            if(obstacleGrid[0][i-1]==1) break;
+            dp[1][i]=1;
+        }
+        for(int i=1;i<=m;i++)
+        {
+            if(obstacleGrid[i-1][0]==1) break;
+            dp[i][1]=1;
+        }
+        for(int i=2;i<=m;i++)
+        {
+            for(int j=2;j<=n;j++)
+            {
+                if(obstacleGrid[i-1][j-1]==1)
+                {
+                    dp[i][j]=0;
+                    continue;
+                }
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m][n];
+    }
+};
