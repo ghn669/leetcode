485141c04839400ec5d9bb7b9c48cf41cf42807b
ghn669 <ghn20061009@qq.com>
2026-04-09 09:26:08 +0800

    343整数拆分

---
+++ 343.cpp
@@ -0,0 +1,18 @@
+//整数拆分
+class Solution {
+public:
+    int integerBreak(int n) {
+        vector<int> dp(n+1);
+        dp[0]=0;
+        dp[1]=0;
+        dp[2]=1;
+        for(int i=3;i<=n;i++)
+        {
+            for(int j=1;j<=i/2;j++)
+            {
+                dp[i]=max(max(j*(i-j),j*dp[i-j]),dp[i]);
+            }
+        }
+        return dp[n];
+    }
+};
