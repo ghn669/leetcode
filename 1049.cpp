f8f16161d6ee4fecb730726b0a4861474bb0a521
ghn669 <ghn20061009@qq.com>
2026-04-09 10:55:39 +0800

    1049最后一块石头的重量

---
+++ 1049.cpp
@@ -0,0 +1,23 @@
+//最后一块石头的重量
+//转换成01背包问题来求解
+class Solution {
+public:
+    int lastStoneWeightII(vector<int>& stones) {
+        int sum = 0;
+        for(int i=0;i<stones.size();i++)
+        {
+            sum+=stones[i];
+        }
+        int target = sum/2;
+        vector<int> dp(target+1,0);
+        for(int i=0;i<stones.size();i++)
+        {
+            for(int j=target;j>=stones[i];j--)
+            {
+                dp[j]=max(dp[j],dp[j-stones[i]]+stones[i]);
+            }
+        }
+        int sub = sum - dp[target]*2;
+        return sub;
+    }
+};
