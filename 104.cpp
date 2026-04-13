c5add632579d3313360336757096f78bb335beca
ghn669 <ghn20061009@qq.com>
2026-04-13 19:54:53 +0800

    104二叉树的最大深度

---
+++ 104.cpp
@@ -0,0 +1,22 @@
+//二叉树的最大深度
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==nullptr) return 0;
+        int m = maxDepth(root->left);
+        int n = maxDepth(root->right);
+        if(m>=n) return m+1;
+        else return n+1;
+    }
+};
