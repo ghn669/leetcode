391be9332d330d78d01afa888d425745acf483b2
ghn669 <ghn20061009@qq.com>
2026-04-19 11:21:22 +0800

    1277统计全为1的正方形的个数

---
+++ 1277.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int res = 0;
+        vector<vector<int>> dp(n,vector<int>(m,0));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(i==0||j==0) dp[i][j]=matrix[i][j];
+                else if(matrix[i][j]==0) dp[i][j]=0;
+                else dp[i][j]=min(min(dp[i-1][j-1],dp[i-1][j]),dp[i][j-1])+1;
+                res+=dp[i][j];
+            }
+        }
+        return res;
+    }
+};
