e9fe7428c4bde902500315131401985eb5da4613
ghn669 <ghn20061009@qq.com>
2026-04-19 11:55:45 +0800

    124二叉树最大路径和

---
+++ 124.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int res = INT_MIN;
+    int dfs(TreeNode* root)
+    {
+        if(root==nullptr) return 0;
+        int left = max(dfs(root->left),0);
+        int right = max(dfs(root->right),0);
+        res = max(res,left+right+root->val);
+        return max(left,right)+root->val;
+    }
+    int maxPathSum(TreeNode* root) {
+        dfs(root);
+        return res;
+    }
+};
